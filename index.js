1c3adeceebc4d2bc30f1966e956c3270a2c4b073
niekaiyu <2932384504@qq.com>
2021-03-02 22:30:14 +0800

    update

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ console.log(123);
 //----------
 console.log(456);
 console.log('hello world');
+
+// git clone 'url'
+// git add index.js
+// git commit -am 'update'
+// git push
+// git branch test
+// git checkout test
+// git merge test
+// git reflog
